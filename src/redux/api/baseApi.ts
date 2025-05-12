a77b5a96fd33fd111f259656f43b85331a1b53b7
Fahima Akhter <sfahima1388@gmail.com>
2025-05-12 10:57:55 +0600

    liveurl type fixed

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 const baseApi = createApi({
   reducerPath: 'baseApi',
   baseQuery: fetchBaseQuery({
-      baseUrl: import.meta.env.VITE_BASE_URL,
+      baseUrl: {`${import.meta.env.VITE_BASE_URL}/api`},
     // baseUrl: 'https://bicycle-pedal-paradise.vercel.app/api',
     credentials: 'include',
   }),
